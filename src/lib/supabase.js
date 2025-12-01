9cd5843cdca8bcc3f3fbe3fd7b851857e581c6c4
ameeetgaikwad <amitmanojgaikwad@gmail.com>
2025-12-01 12:00:00 -0300

    Add tests in supabase.js

---
+++ src/lib/supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from "@supabase/supabase-js";
+
+export const supabase = createClient(
+  "https://ubmxnowtwnubrprmdngx.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVibXhub3d0d251YnJwcm1kbmd4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI5NjQwMTksImV4cCI6MjA3ODU0MDAxOX0.96F-ZBariXzIB4q5BNywyNkdN0efwsFjDiNYQaRCZ0g"
+);
